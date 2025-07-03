d73a3bd10b379d1368b2bccad63d2f8346e33516
RafaelMagnago <rmagnago@outlook.com>
2025-07-02 23:46:11 -0300

    atualização dos links de registro na barra superior

--- src/components/topBarHome.tsx
+++ src/components/topBarHome.tsx
@@ -6,9 +6,9 @@ export function TopBarHome() {
                 <h1 className="text-xl font-bold text-[#195245]">PetConnect</h1>
             </div>
             <nav className="flex gap-4 text-sm items-center">
-                <a href="/register" className="hover:underline">Criar perfil</a>
+                <a href="/register/tutor" className="hover:underline">Criar perfil</a>
                 <a href="/login" className="hover:underline">Entrar</a>
-                <a href="#" className="bg-[#046C5E] text-white px-4 py-2 rounded-full hover:bg-[#03584d]">Trabalhe conosco</a>
+                <a href="/register/medico" className="bg-[#046C5E] text-white px-4 py-2 rounded-full hover:bg-[#03584d]">Trabalhe conosco</a>
             </nav>
         </header >
     );
